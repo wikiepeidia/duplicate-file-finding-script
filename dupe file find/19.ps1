d04deb5757d68710fdfcf5e79f45446208b61c5e
pham the minh <phamtheminh2409@gmail.com>
2024-01-08 13:48:19 +0700

    Add files via upload

---
+++ dupe file find/19.ps1
@@ -0,0 +1,80 @@
+function Get-FileContentHash($filePath) {
+    try {
+        $stream = [System.IO.File]::OpenRead($filePath)
+        $hasher = Get-FileHash -InputStream $stream -Algorithm SHA256
+        $stream.Close()
+        return $hasher.Hash
+    }
+    catch {
+        Write-Warning "Failed to read file: $filePath"
+        return "unreadable_file"
+    }
+}
+
+function Find-Duplicates($directory) {
+    $groupedFiles = Get-ChildItem -File -Recurse $directory | Group-Object Extension
+
+    $duplicates = @()
+    $processedFiles = @{}
+
+    foreach ($group in $groupedFiles) {
+        $sizeGroups = $group.Group | Group-Object Length
+
+        foreach ($sizeGroup in $sizeGroups) {
+            if ($sizeGroup.Count -gt 1) {
+                $contentHashes = @{}
+
+                foreach ($file in $sizeGroup.Group) {
+                    $contentHash = Get-FileContentHash $file.FullName
+
+                    if (-not $contentHashes.ContainsKey($contentHash)) {
+                        $contentHashes[$contentHash] = @()
+                    }
+
+                    $contentHashes[$contentHash] += $file.FullName
+                }
+
+                foreach ($hashValue in $contentHashes.Keys) {
+                    $duplicateFiles = $contentHashes[$hashValue]
+
+                    if ($duplicateFiles.Count -gt 1) {
+                        $duplicateGroup = @()
+                        foreach ($firstFile in $duplicateFiles) {
+                            if (-not $processedFiles.ContainsKey($firstFile)) {
+                                foreach ($duplicateFile in $duplicateFiles) {
+                                    if ($firstFile -ne $duplicateFile) {
+                                        $duplicateGroup += $duplicateFile
+                                        $processedFiles[$duplicateFile] = $true
+                                    }
+                                }
+                                Write-Host "$firstFile is a duplicate of $($duplicateGroup -join ' and ')"
+                                $duplicates += $firstFile
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    return $duplicates
+}
+
+
+if ($args.Count -ne 1) {
+    Write-Host "Usage: script.ps1 'C:\path\to\your\files'"
+    exit 1
+}
+
+$directoryPath = $args[0]
+
+if (-not (Test-Path $directoryPath -PathType Container)) {
+    Write-Host "Invalid directory path."
+    exit 1
+}
+
+$duplicatesFound = Find-Duplicates -directory $directoryPath
+
+if (-not $duplicatesFound) {
+    Write-Host "No duplicate files found."
+}
